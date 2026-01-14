4d08348a7098e541a6eb976a30cebdb4166ca40e
Marcelo Henrique <marcelo.hcns@gmail.com>
2026-01-14 10:11:28 -0300

    wip: Basic character movement

---
+++ Source/SurvivalDemios/TPSCharacter.cpp
@@ -0,0 +1,127 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TPSCharacter.h"
+#include "Components/InputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/PawnMovementComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Components/CapsuleComponent.h"
+
+// Sets default values
+ATPSCharacter::ATPSCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	SpringArmCamera = CreateDefaultSubobject<USpringArmComponent>(FName("Spring Arm Component"));
+	SpringArmCamera->TargetArmLength = 200.f;
+	SpringArmCamera->bUsePawnControlRotation = true;
+	SpringArmCamera->AddRelativeLocation(FVector(0.f, 40.f, 50.f));
+	SpringArmCamera->SetupAttachment(RootComponent);
+
+	CharacterCamera = CreateDefaultSubobject<UCameraComponent>(FName("CharacterCamera"));
+	CharacterCamera->SetupAttachment(SpringArmCamera);
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0; // change it in case of multiplayer
+}
+
+void ATPSCharacter::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	GetMovementComponent()->GetNavAgentPropertiesRef().bCanCrouch = true;
+
+	CharacterMovement = GetCharacterMovement();
+	CharacterMovement->JumpZVelocity = 600.f;
+	CharacterMovement->GravityScale = 1.5f;
+
+	SpringArmCamera->bEnableCameraLag = true;
+	SpringArmCamera->CameraLagSpeed = 4.f;
+}
+
+// Called when the game starts or when spawned
+void ATPSCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void ATPSCharacter::CrouchToggle()
+{
+	if (IsCrouched())
+	{
+		UnCrouch();
+	}
+	else
+	{
+		Crouch();
+	}
+}
+
+void ATPSCharacter::CrouchHold()
+{
+	Crouch();
+}
+
+void ATPSCharacter::StandUp()
+{
+	UnCrouch();
+}
+
+// Called every frame
+void ATPSCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ATPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis("MoveForward", this, &ATPSCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("Sidewalk", this, &ATPSCharacter::Sidewalk);
+	PlayerInputComponent->BindAxis("MouseY", this, &ATPSCharacter::VerticalLook);
+	PlayerInputComponent->BindAxis("MouseX", this, &ATPSCharacter::HorizontalLook);
+
+	PlayerInputComponent->BindAction("Crouch", IE_Pressed, this, &ATPSCharacter::CrouchHold);
+	PlayerInputComponent->BindAction("Crouch", IE_Released, this, &ATPSCharacter::StandUp);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ATPSCharacter::StartJump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ATPSCharacter::StopJump);
+
+	PlayerInputComponent->BindAction("CrouchToggle", IE_Pressed, this, &ATPSCharacter::CrouchToggle);
+}
+
+void ATPSCharacter::MoveForward(float Value)
+{
+	AddMovementInput(GetActorForwardVector(), Value, true);
+}
+
+void ATPSCharacter::Sidewalk(float Value)
+{
+	AddMovementInput(GetActorRightVector(), Value, true);
+}
+
+void ATPSCharacter::VerticalLook(float Value)
+{
+	AddControllerPitchInput(-Value);
+}
+
+void ATPSCharacter::HorizontalLook(float Value)
+{
+	AddControllerYawInput(Value);
+}
+
+void ATPSCharacter::StartJump()
+{
+	bIsJumping = true;
+	Jump();
+}
+
+void ATPSCharacter::StopJump()
+{
+	bIsJumping = false;
+	StopJumping();
+}
+
---
+++ Source/SurvivalDemios/TPSCharacter.h
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "TPSCharacter.generated.h"
+
+UCLASS()
+class SURVIVALDEMIOS_API ATPSCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ATPSCharacter();
+
+	virtual void PostInitializeComponents() override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable)
+	void MoveForward(float Value);
+
+	UFUNCTION(BlueprintCallable)
+	void Sidewalk(float Value);
+
+	UFUNCTION(BlueprintCallable)
+	void VerticalLook(float Value);
+
+	UFUNCTION(BlueprintCallable)
+	void HorizontalLook(float Value);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	class UCameraComponent* CharacterCamera;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	class USpringArmComponent* SpringArmCamera;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Jump")
+	bool bIsJumping;
+
+	class UCharacterMovementComponent* CharacterMovement;
+
+	void StartJump();
+	void StopJump();
+
+	void CrouchToggle();
+
+	void CrouchHold();
+
+	void StandUp();
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void OnComponentHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	void OnComponentBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+};
