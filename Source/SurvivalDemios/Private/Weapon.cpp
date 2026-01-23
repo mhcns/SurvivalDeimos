2fbb47350516056c1d14505fca2a8c5749266aa6
Marcelo Henrique <marcelo.hcns@gmail.com>
2026-01-22 22:56:34 -0300

    weapon trail, muzzle and hit effects

--- Source/SurvivalDemios/Private/Weapon.cpp
+++ Source/SurvivalDemios/Private/Weapon.cpp
@@ -9,6 +9,8 @@
 #include "DrawDebugHelpers.h"
 #include "UObject/ConstructorHelpers.h"
 #include "Engine/SkeletalMesh.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AWeapon::AWeapon()
@@ -16,11 +18,29 @@ AWeapon::AWeapon()
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>(FName("WeaponMesh"));
+	MuzzleFlashEffect = CreateDefaultSubobject<UParticleSystemComponent>(FName("MuzzleFlashEffect"));
+	BulletTrail = CreateDefaultSubobject<UParticleSystemComponent>(FName("BulletTrail"));
+
 	ConstructorHelpers::FObjectFinder<USkeletalMesh> WeaponMeshAsset(TEXT("/Script/Engine.SkeletalMesh'/Game/Weapons/Rifle.Rifle'"));
 
-	if (WeaponMeshAsset.Succeeded())
+	ConstructorHelpers::FObjectFinder<UParticleSystem> MuzzleFlashAsset(TEXT("/Script/Engine.ParticleSystem'/Game/Effects/ParticleSystems/Weapons/AssaultRifle/Muzzle/P_AssaultRifle_MF.P_AssaultRifle_MF'"));
+
+	ConstructorHelpers::FObjectFinder<UParticleSystem> BulletTrailAsset(TEXT("/Script/Engine.ParticleSystem'/Game/Effects/ParticleSystems/Weapons/AssaultRifle/Muzzle/P_AssaultRifle_Trail.P_AssaultRifle_Trail'"));
+
+	ConstructorHelpers::FObjectFinder<UParticleSystem> BulletImpactAsset(TEXT("/Script/Engine.ParticleSystem'/Game/Effects/ParticleSystems/Weapons/AssaultRifle/Impacts/P_AssaultRifle_IH.P_AssaultRifle_IH'"));
+
+	//ConstructorHelpers::FObjectFinder<UParticleSystem> BloodImpactAsset(TEXT(""));
+
+	if (WeaponMeshAsset.Succeeded() && MuzzleFlashAsset.Succeeded() && BulletTrailAsset.Succeeded() && BulletImpactAsset.Succeeded())
 	{
 		WeaponMesh->SetSkeletalMesh(WeaponMeshAsset.Object);
+		MuzzleFlashEffect->SetTemplate(MuzzleFlashAsset.Object);
+		BulletTrail->SetTemplate(BulletTrailAsset.Object);
+		BulletImpactFX = BulletImpactAsset.Object;
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("One or more FObjectFinders failed to retrieve an asset"));
 	}
 
 	RootComponent = WeaponMesh;
@@ -30,6 +50,18 @@ AWeapon::AWeapon()
 	MuzzleArrow->SetRelativeLocation(FVector(-4.f, 0, -1.2f));
 	MuzzleArrow->SetRelativeScale3D(FVector(0.3f, 0.7f, 0.7f));
 
+	MuzzleFlashEffect->SetupAttachment(MuzzleArrow);
+	MuzzleFlashEffect->bAutoActivate = false;
+	MuzzleFlashEffect->SetRelativeScale3D(FVector(3.f, 3.f, 3.f));
+
+	BulletTrail->SetupAttachment(MuzzleArrow);
+	BulletTrail->bAutoActivate = false;
+}
+
+void AWeapon::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	BulletTrail->SetTrailSourceData(FName("MuzzleFlashSocket"), FName("MuzzleFlashSocket2"), ETrailWidthMode_FromCentre, 2.f);
 }
 
 // Called when the game starts or when spawned
@@ -51,8 +83,10 @@ void AWeapon::Fire()
 	if (MuzzleArrow)
 	{
 		FVector MuzzleLocation = MuzzleArrow->GetComponentLocation();
-		FVector MuzzleDirection = MuzzleArrow->GetComponentRotation().Vector();
+		FRotator MuzzleRotator = MuzzleArrow->GetComponentRotation();
+		FVector MuzzleDirection = MuzzleRotator.Vector();
 		FVector EndLocation = MuzzleLocation + (MuzzleDirection * 1000.f);
+		float Distance = 1000.f;
 
 		FHitResult HitResult;
 		FCollisionQueryParams QueryParams;
@@ -65,8 +99,34 @@ void AWeapon::Fire()
 		if (bHit)
 		{
 			GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Red, TEXT("Hit"));
+			EndLocation = HitResult.Location;
+			Distance = HitResult.Distance;
+			CheckBulletHit(HitResult);
 		}
 
-		DrawDebugLine(GetWorld(), MuzzleLocation, EndLocation, FColor::Green, false, 1.f, 0, 1.f);
+		MuzzleFlashEffect->ActivateSystem();
+		BulletTrail->SetVectorParameter("ShockBeamEnd", EndLocation);
+		//Trail->SetFloatParameter("BeamLength", Distance);
+		BulletTrail->ActivateSystem(); // depois fazer o tiro sair na mira da câmera
+
+		//DrawDebugLine(GetWorld(), MuzzleLocation, EndLocation, FColor::Green, false, 1.f, 0, 1.f);
+	}
+}
+
+void AWeapon::CheckBulletHit(FHitResult HitResult)
+{
+	if (HitResult.GetActor()->Tags.Contains(TEXT("Enemy")))
+	{
+		// Spawn blood effect;
+	}
+	else
+	{
+		UParticleSystemComponent* HitFX =
+			UGameplayStatics::SpawnEmitterAtLocation(
+				GetWorld(),
+				BulletImpactFX,
+				HitResult.Location,
+				HitResult.ImpactNormal.Rotation()
+			);
 	}
 }
--- Source/SurvivalDemios/Public/Weapon.h
+++ Source/SurvivalDemios/Public/Weapon.h
@@ -15,6 +15,8 @@ public:
 	// Sets default values for this actor's properties
 	AWeapon();
 
+	virtual void PostInitializeComponents() override;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -24,6 +26,20 @@ protected:
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon", meta = (AllowPrivateAccess = "true"))
 	class UArrowComponent* MuzzleArrow;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WeaponFX")
+	class UParticleSystemComponent* MuzzleFlashEffect;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WeaponFX")
+	class UParticleSystemComponent* BulletTrail;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "WeaponFX")
+	class UParticleSystem* BulletImpactFX;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "WeaponFX")
+	class UParticleSystem* BloodImpactFX;
+
+	void CheckBulletHit(FHitResult HitResult);
 
 public:
 	// Called every frame
